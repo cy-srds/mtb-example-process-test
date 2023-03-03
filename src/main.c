dabac515efc4d7d526136a5bd5b91e51c92ad753
Srikanth D S <srikanth.ds@infineon.com>
2023-03-04 00:59:38 +0530

    CAPSENSE Buttons and Slider port to Zephyr (#1)
    * CAPSENSE Buttons and Slider port to Zephyr
    
    * Added MTB 2.4 Generated files
    
    * Removed debug print messages and unnecessary files
    
    * Added an overlay file and set define to use custom design file
    
    * FreeRTOS version of the CE ported to Zephyr
    
    * Fixed couple of issues
    
    * Use FIFO instead of Queue
    
    * Updated the CapSense example path and README
    
    * Set Debug optmization for build

---
+++ src/main.c
@@ -0,0 +1,112 @@
+/******************************************************************************
+* File Name: main.c
+*
+* Description: This code example features a 5-segment CAPSENSE slider and two
+*              CAPSENSE buttons. Button 0 turns the LED ON, Button 1 turns the
+*              LED OFF, and the slider controls the brightness of the LED. The
+*              code example also features interfacing with Tuner GUI using I2C
+*              interface.
+*
+*              This code example uses Zephyr. For documentation and API
+*              references of Zephyr, visit : https://docs.zephyrproject.org
+*
+* Related Document: See README.md
+*
+*
+*******************************************************************************
+* Copyright 2019-2023, Cypress Semiconductor Corporation (an Infineon company) or
+* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
+*
+* This software, including source code, documentation and related
+* materials ("Software") is owned by Cypress Semiconductor Corporation
+* or one of its affiliates ("Cypress") and is protected by and subject to
+* worldwide patent protection (United States and foreign),
+* United States copyright laws and international treaty provisions.
+* Therefore, you may use this Software only as provided in the license
+* agreement accompanying the software package from which you
+* obtained this Software ("EULA").
+* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
+* non-transferable license to copy, modify, and compile the Software
+* source code solely for use in connection with Cypress's
+* integrated circuit products.  Any reproduction, modification, translation,
+* compilation, or representation of this Software except as specified
+* above is prohibited without the express written permission of Cypress.
+*
+* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
+* reserves the right to make changes to the Software without notice. Cypress
+* does not assume any liability arising out of the application or use of the
+* Software or any product or circuit described in the Software. Cypress does
+* not authorize its products for use in any products where a malfunction or
+* failure of the Cypress product may reasonably be expected to result in
+* significant property damage, injury or death ("High Risk Product"). By
+* including Cypress's product in a High Risk Product, the manufacturer
+* of such system or application assumes all risk of such use and in doing
+* so agrees to indemnify Cypress against all liability.
+*******************************************************************************/
+
+#include "cybsp.h"
+#include "capsense_task.h"
+#include "led_task.h"
+#include <zephyr/device.h>
+
+
+/*******************************************************************************
+ * Global constants
+ ******************************************************************************/
+/* Priorities of user tasks in this project. */
+#define TASK_CAPSENSE_PRIORITY   (6u)
+#define TASK_LED_PRIORITY        (5u)
+
+/* Stack sizes of user tasks in this project */
+#define TASK_CAPSENSE_STACK_SIZE (1024u)
+#define TASK_LED_STACK_SIZE      (512u)
+
+
+/*******************************************************************************
+ * Global variable
+ ******************************************************************************/
+/* Create the threads. See the respective task definition for more
+    * details of these tasks.
+    */
+K_THREAD_DEFINE(task_capsense_id, TASK_CAPSENSE_STACK_SIZE, task_capsense,
+            NULL, NULL, NULL, TASK_CAPSENSE_PRIORITY, 0, -1);
+K_THREAD_DEFINE(task_led_id, TASK_LED_STACK_SIZE, task_led,
+            NULL, NULL, NULL, TASK_LED_PRIORITY, 0, -1);
+
+
+/*******************************************************************************
+* Function Name: main()
+********************************************************************************
+* Summary:
+*  System entrance point. This function sets up user tasks and then starts
+*  the RTOS scheduler.
+*
+* Return:
+*  int
+*
+*******************************************************************************/
+void main(void)
+{
+    cy_rslt_t result;
+
+    /* Initialize the device and board peripherals */
+    result = cybsp_init();
+
+    /* Board init failed. Stop program execution */
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
+
+    /* Enable global interrupts */
+    __enable_irq();
+
+    /* Started the created threads */
+    k_thread_start(task_capsense_id);
+    k_thread_start(task_led_id);
+}
+
+
+/* [] END OF FILE */
